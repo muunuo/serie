b8ee94bb0319e4e296cb8d6dbb3cc56f43726e9e
muunuo <benkforland@gmail.com>
2026-03-25 14:23:58 +0100

    prøver å fikse en dum feil

--- app.js
+++ app.js
@@ -9,18 +9,20 @@ Working on:
 */
 
 const express = require('express');
-const mysql = require('mysql2/promise'); // trengs grunnet bruk av Maria.db. Lastes ned som mysql2
-const bcrypt = require("bcrypt"); //to be able to protect passwords
 const app = express();
-const port = 3000; // Hvilken port det åpned 
+const mysql = require('mysql2/promise'); // needed do to mariadb (download as mysql2)
+const bcrypt = require('bcrypt'); //to be able to protect passwords
+const session = require('express-session');
+const port = 3000; // What port the site uses
 
-const pool = mysql.createPool({ // trengs grunnet bruk av Maria.db.
+const pool = mysql.createPool({ // needed do to mariadb
   host: '127.0.0.1',
   port: 3306,
   user: 'root',
   password: 'root',
   database: 'serie',
   connectionLimit: 5,
+  multipleStatements: true
 });
 
 /*
@@ -70,64 +72,72 @@ CREATE TABLE IF NOT EXISTS status_serie (
 );
 `);
 
+app.use(
+  session({
+    secret: "secretKey", //incrypt the session-ID (make sure to change)
+    resave: false, // Won't save a unchanged session
+    saveUninitialized: false, // Won't save a empty session
+    cookie: {
+      secure: false, // change to 'true' if using https
+      maxAge: 1000 * 60 * 60 // session expier after 1 houre 
+    }
+  })
+);
+
 app.use(express.static('public')); //Allows you to get files from public
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.json()); // middleware for parse JSON from request body
+
+const path = require('path'); //to handle filepaths 
+// app.use(express.urlencoded({ extended: true})); //lets you get info from users serchbare (like id)
 
 function requireLogin_(req, res, next) {
-  if (!req.session.bruker) {
+  if (!req.session.bruker) { // if users isn't logde in (has a session), then redirect to startsite
     return res.redirect("/");
   }
-  next();
+  next(); //is user has session, they can continue on
 }
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 }); 
 
-app.get('/user', async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT * FROM bruker');
-    console.log(rows);
-    res.json(rows);
-  } catch (err) {
-    console.error('Database error:', err);
-    res.status(500).json({ error: 'Failed to fetch data from serie_database_1.serie' });
-  }
-});
+// app.use('/private', requireLogin_, (req, res) => {
+//   const userID_ = req.session.user.id;
+//   const [user_] = await pool.query('SELECT bruker_id, brukernavn, kallenavn, passord FROM bruker WHERE brukernavn = ?').get(userID_);
+// })
 
-app.post('/createUser_', async (req, res) => {
-  const { username_, password_, nickname_ } = req.body;
 
-  const [rows_] = await pool.query('SELECT * FROM bruker WHERE brukernavn = ?', [username_]); //rows_ checks if the username is taken or free. the [] is because it is checking multiple rows. (maria.db exlusiv)
-  if (rows_.length > 0) {
-    return res.status(400).json({ message: "Brukernavn eksisterer allerede. Velg et annet brukernavn."});
-  }
+// app.get('/user', async (req, res) => {
+//   try {
+//     const [rows] = await pool.query('SELECT * FROM bruker');
+//     console.log(rows);
+//     res.json(rows);
+//   } catch (err) {
+//     console.error('Database error:', err);
+//     res.status(500).json({ error: 'Failed to fetch data from serie_database_1.serie' });
+//   }
+// });
 
-    try {
-    const [result] = await pool.execute( //it is again using multiple rows, so it uses []. 
-      'INSERT INTO bruker (brukernavn, passord, kallenavn) VALUES (?, ?, ?)', 
-      [username_, password_, nickname_] // insted of run and get you just use a comma and [] to send quarys to the database. 
-    );
-    res.status(201).json({ message: "Konto opprettet!", id: result.insertId });
-  } catch (error){
-    console.log(error);
-    res.status(500).json({message: "Feil med inlogging"});
-  }
-});
+// app.post('/createUser_', async (req, res) => {
+//   const { username_, password_, nickname_ } = req.body;
+
+//   const [rows_] = await pool.query('SELECT * FROM bruker WHERE brukernavn = ?', [username_]); //rows_ checks if the username is taken or free. the [] is because it is checking multiple rows. (maria.db exlusiv)
+//   if (rows_.length > 0) {
+//     return res.status(400).json({ message: "Brukernavn eksisterer allerede. Velg et annet brukernavn."});
+//   }
+
+//     try {
+//     const [result] = await pool.execute( //it is again using multiple rows, so it uses []. 
+//       'INSERT INTO bruker (brukernavn, passord, kallenavn) VALUES (?, ?, ?)', 
+//       [username_, password_, nickname_] // insted of run and get you just use a comma and [] to send quarys to the database. 
+//     );
+//     res.status(201).json({ message: "Konto opprettet!", id: result.insertId });
+//   } catch (error){
+//     console.log(error);
+//     res.status(500).json({message: "Feil med inlogging"});
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`website running at http://localhost:${port}`);
 });
-
-/*
-brukernavn = username
-passord = password
-kallenavn = nickname 
-skjema = form
-send = submitt
-
-Changed to english:
-kravInlogging = requireLogin_
-opprettBruker = createUser_
-*/
